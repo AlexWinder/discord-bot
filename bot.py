b4171aeb7c70c425e3a7c52e719d57c718da5c62
Alex Winder <contact@alexwinder.uk>
2022-06-22 19:51:40 +0100

    Specify the joke type

--- bot.py
+++ bot.py
@@ -10,6 +10,8 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 client = discord.Client()
 
+allowedJokeTypes = ['Any', 'Misc', 'Programming', 'Dark', 'Pun', 'Spooky', 'Christmas']
+
 @client.event
 async def on_message(message):
     # Avoid itself
@@ -20,26 +22,40 @@ async def on_message(message):
         await message.channel.send(
             "I currently understand the following commands:\n"
             "!help - Displays a list of all commands supported by me.\n"
-            "!joke - I tell a random joke.\n"
+            "!joke - I tell a random joke. You can specify the type of joke by appending with one of the following: " + ' '.join(allowedJokeTypes) + "\n"
         )
 
-    if message.content == '!joke':
-        response = requests.get('https://v2.jokeapi.dev/joke/Any')
-        parsed = response.json()
+    if message.content.startswith('!joke'):
+        jokeType = None
+        jokeMessage = message.content.split()
 
-        if parsed['error'] == False:
-            await message.channel.send("Here's a cool " + parsed['category'] + " joke.")
+        if len(jokeMessage) < 2:
+            jokeType = 'Any'
 
-            if 'setup' in parsed and 'delivery' in parsed:
-                await message.channel.send(parsed['setup'])
-                time.sleep(1)
-                await message.channel.send(parsed['delivery'])
-            elif 'joke' in parsed:
-                await message.channel.send(parsed['joke'])
+        if len(jokeMessage) >= 2 and jokeMessage[1].capitalize() not in allowedJokeTypes:
+            await message.channel.send("I don't understand that joke type. Accepted types are: " + ' '.join(allowedJokeTypes))
+            return
+
+        if len(jokeMessage) >= 2 and jokeMessage[1]:
+            jokeType = jokeMessage[1].capitalize()
+
+        if jokeType:
+            response = requests.get('https://v2.jokeapi.dev/joke/' + jokeType)
+            parsed = response.json()
+
+            if parsed['error'] == False:
+                await message.channel.send("Here's a cool " + parsed['category'] + " joke.")
+
+                if 'setup' in parsed and 'delivery' in parsed:
+                    await message.channel.send(parsed['setup'])
+                    time.sleep(1)
+                    await message.channel.send(parsed['delivery'])
+                elif 'joke' in parsed:
+                    await message.channel.send(parsed['joke'])
+                else:
+                    await message.channel.send("Weird, I've forgotton it already...")
             else:
-                await message.channel.send("Weird, I've forgotton it already...")
-        else:
-            await message.channel.send('I have no jokes for you right now. Try again later.')
+                await message.channel.send('I have no jokes for you right now. Try again later.')
 
 # Bot token
 client.run(BOT_TOKEN)
